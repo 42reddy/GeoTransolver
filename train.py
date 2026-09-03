4a3a9ba7d4764f80d0b4d57ed139be6404798317
42reddy <110433102+42reddy@users.noreply.github.com>
2026-09-03 17:34:06 +0530

    added geotransolver

--- train.py
+++ train.py
@@ -64,7 +64,7 @@ RESUME = None          # Path to a checkpoint .pt to resume from, or None
 
 # optimization
 EPOCHS = 25
-BATCH_SIZE = 4
+BATCH_SIZE = 6
 LR = 3e-4
 WEIGHT_DECAY = 1e-4
 WARMUP_FRAC = 0.05
